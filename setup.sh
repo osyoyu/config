a66d1dbcfeaa0bcc4d9f83d1c28e53e281468b7c
osyoyu <osyoyu@osyoyu.com>
2015-04-10 19:44:44 +0900

    Fix path to .tmux.conf in setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ ln -sf ${DIR}/zsh/dot.zlogout ${HOME}/.zlogout
 ln -sf ${DIR}/vim/dot.vimrc ${HOME}/.vimrc
 ln -sf ${DIR}/vim/dot.vim ${HOME}/.vim
 
-ln -sf ${DIR}/dot.tmux.conf ${HOME}/.tmux.conf
+ln -sf ${DIR}/tmux/dot.tmux.conf ${HOME}/.tmux.conf
 
 ln -sf ${DIR}/git/dot.gitconfig ${HOME}/.gitconfig
 ln -sf ${DIR}/git/dot.gitignore ${HOME}/.gitignore
