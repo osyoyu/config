b70fb6302c850ef7d632f3d3a7adad90269428ad
osyoyu <osyoyu@osyoyu.com>
2016-02-01 11:31:41 +0900

    setup-osx: 勝手に引用符を変換するな

--- setup-osx.sh
+++ setup-osx.sh
@@ -5,4 +5,11 @@ defaults write com.apple.screencapture name "Screenshot"
 defaults write com.apple.screencapture location ${HOME}/Dropbox/Screenshots/`hostname | sed -e 's/\.local//g'`
 defaults write com.apple.screencapture disable-shadow -bool true
 defaults write -g ApplePersistence -bool false
+
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+defaults write -g CheckSpellingWhileTyping -bool false
+defaults write -g NSAllowContinuousSpellChecking -bool false
+
 killall SystemUIServer
