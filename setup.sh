0e7256401b59991c6511fee7613d57f6438eb4f1
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-02-15 18:37:23 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,23 @@ set -eu -o pipefail
 
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+set -x
+
 ln -sf ${DIR}/bash/dot.bashrc ${HOME}/.bashrc
 
 ln -sf ${DIR}/zsh/dot.zshrc ${HOME}/.zshrc
 ln -sf ${DIR}/zsh/dot.zlogout ${HOME}/.zlogout
 
 ln -sf ${DIR}/vim/dot.vimrc ${HOME}/.vimrc
-ln -sf ${DIR}/vim/dot.vim ${HOME}/.vim
+ln -sfn ${DIR}/vim/dot.vim ${HOME}/.vim
+
+mkdir -p ${HOME}/.config/nvim
+ln -sf ${DIR}/nvim/init.lua ${HOME}/.config/nvim/init.lua
 
 ln -sf ${DIR}/tmux/dot.tmux.conf ${HOME}/.tmux.conf
 
 ln -sf ${DIR}/git/dot.gitconfig ${HOME}/.gitconfig
 ln -sf ${DIR}/git/dot.gitignore ${HOME}/.gitignore
+
+mkdir -p ${HOME}/.config/ghostty
+ln -sf ${DIR}/ghostty/config ${HOME}/.config/ghostty/config
