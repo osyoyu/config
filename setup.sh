f26ba3f0be9498ce0839d5cc83496f9049afca46
Daisuke Aritomo <osyoyu@osyoyu.com>
2026-09-09 14:15:11 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,3 +28,6 @@ ln -sf ${DIR}/zed/settings.json ${HOME}/.config/zed/settings.json
 ln -sf ${DIR}/zed/keymap.json ${HOME}/.config/zed/keymap.json
 
 ln -sf ${DIR}/claude/settings.json ${HOME}/.claude/settings.json
+ln -sf ${DIR}/claude/CLAUDE.md ${HOME}/.claude/CLAUDE.md
+
+ln -sf ${DIR}/agents/plugins/marketplace.json ${HOME}/.agents/plugins/marketplace.json
